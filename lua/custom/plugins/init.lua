f7f6700f76c4bf4e94a16e4d4de2755cf7b37ff3
seabrus <g.seabra@protonmail.com>
2024-08-06 00:07:56 +0100

    neotree reset

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,16 +5,6 @@
 return {
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-  },
-  {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {
